4d7ac83fe1c5d5d52a8f303acee77fef1bf4e03c
Artem Potemkin <artem.potemkin@konecranes.com>
2019-07-15 17:01:02 +0300

    Added part2

---
+++ part2/courseinfo/build/precache-manifest.12deda6a1e51b75731cf3e1f9c782879.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083b53f53cccaee7411b74879e96a192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "983af3329853fbfd4111",
+    "url": "/static/js/main.ab8fb6ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
