453387a5992c6a0c0d749b5131a212918bbdf05a
Artem Potemkin <artem.potemkin@konecranes.com>
2019-07-28 17:09:00 +0300

    Work in progress

---
+++ part4/blog/controllers/blog.js
@@ -0,0 +1,12 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+module.exports = blogRouter
