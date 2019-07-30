6bcf4e4b5ab120499fc21b41e145fe4eed90f618
Artem Potemkin <artem.potemkin@konecranes.com>
2019-07-30 16:50:05 +0300

    Work in progress

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -22,41 +22,41 @@ const initialBlog = [
       "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
     __v: 0
-  }
-  //   {
-  //     _id: "5a422b3a1b54a676234d17f9",
-  //     title: "Canonical string reduction",
-  //     author: "Edsger W. Dijkstra",
-  //     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-  //     likes: 12,
-  //     __v: 0
-  //   },
-  //   {
-  //     _id: "5a422b891b54a676234d17fa",
-  //     title: "First class tests",
-  //     author: "Robert C. Martin",
-  //     url:
-  //       "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-  //     likes: 10,
-  //     __v: 0
-  //   },
-  //   {
-  //     _id: "5a422ba71b54a676234d17fb",
-  //     title: "TDD harms architecture",
-  //     author: "Robert C. Martin",
-  //     url:
-  //       "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-  //     likes: 0,
-  //     __v: 0
-  //   },
-  //   {
-  //     _id: "5a422bc61b54a676234d17fc",
-  //     title: "Type wars",
-  //     author: "Robert C. Martin",
-  //     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-  //     likes: 2,
-  //     __v: 0
-  //   }
+  },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url:
+        "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url:
+        "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }
 ]
 
 beforeEach(async () => {
@@ -86,6 +86,13 @@ test("A specific author is within the returned posts", async () => {
   const author = response.body.map(r => r.author)
 
   expect(author).toContain("Michael Chan")
+})
+
+test("A unique identifier is named id", async () => {
+  const response = await api.get("/api/blog")
+  response.body.map(b => {
+    expect(b.id).toBeDefined()
+  })
 })
 
 test("A valid post can be added ", async () => {
