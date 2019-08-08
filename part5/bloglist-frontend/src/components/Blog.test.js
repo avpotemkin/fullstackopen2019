af3611f963e59549d5cb8191d5083bc679737de8
Artem Potemkin <artem.potemkin@konecranes.com>
2019-08-08 16:53:17 +0300

    Work in progress

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import render from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
+import Togglable from './Togglable'
+
+afterEach(cleanup)
 
 test('renders content', () => {
   const user = {
@@ -22,9 +26,38 @@ test('renders content', () => {
   const component = render(<Blog blog={blog} user={user} />)
 
   const div = component.container.querySelector('.blog')
-  console.log(prettyDOM(div))
 
-  //   expect(div).toHaveTextContent(
-  //     'Component testing is done with react-testing-library'
-  //   )
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
 })
+
+test('clicking the button twice calls event handler two times', async () => {
+  const user = {
+    id: 123456,
+    username: 'testuser'
+  }
+
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'me',
+    user: {
+      id: 123456,
+      username: 'testuser'
+    }
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <Blog blog={blog} user={user} handleLikeButton={mockHandler} />
+  )
+
+  const likeButton = getByText('like')
+
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
+})
+
